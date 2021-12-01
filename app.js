79d80b2a4e1dbaa144186bb26ba02b7057627b0d
Rebecca Hjörvar <rebecca.hjorvarroutlook.com>
2021-12-01 14:33:11 +0100

    Set up MongoDB

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const url = 'mongodb://localhost:27017';
 const dbName = 'fruitsDB';
 
 // Create a new MongoClient
-const client = new MongoClient(url);
+const client = new MongoClient(url, { useNewUrlParser: true });
 
 // Use connect method to connect to the Server
 client.connect(function(err) {
@@ -18,6 +18,49 @@ client.connect(function(err) {
   console.log("Connected successfully to server");
 
   const db = client.db(dbName);
-
-  client.close();
+  
+  findDocuments(db, () => {
+    client.close();
+  });
 });
+
+const insertDocuments = function(db, callback) {
+    // Get the documents collection
+    const collection = db.collection('fruits');
+    // Insert some documents
+    collection.insertMany([
+      {
+        name: "Apple",
+        score: 8,
+        review: "Great fruit"
+      },
+      {
+          name: "Orange",
+          score: 6,
+          review: "Kinda sour" 
+      },
+      {
+          name:"Banana",
+          score: 9,
+          review: "Greate stuff!"
+      }
+    ], function(err, result) {
+      assert.equal(err, null);
+      assert.equal(3, result.result.n);
+      assert.equal(3, result.ops.length);
+      console.log("Inserted 3 documents into the collection");
+      callback(result);
+    });
+  }
+
+  const findDocuments = function(db, callback) {
+    // Get the documents collection
+    const collection = db.collection('fruits');
+    // Find some documents
+    collection.find({}).toArray(function(err, fruits) {
+      assert.equal(err, null);
+      console.log("Found the following records");
+      console.log(fruits)
+      callback(fruits);
+    });
+  }
